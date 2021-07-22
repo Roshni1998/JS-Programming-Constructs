be3090e4b3d7a699d6b754497a1eb0da59b066c2
Roshni1998 <roshnimali847@gmail.com>
2021-07-23 01:13:01 +0530

    Added: UC-1 Check Employee Presence

---
+++ EmployeeWage.js
@@ -0,0 +1,11 @@
+//UC-1 Check Employee Presence
+
+const IS_ABSENT = 0
+
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == IS_ABSENT) {
+	console.log("Employee is ABSENT");
+	return;
+} else {
+	console.log("Employee is PRESENT");
+}
